036250c4d3c8edb2fcccd6cfb0fabf91745ce844
Sophie Wong <moogi27@gmail.com>
2026-05-16 11:53:48 +1000

    adding contact us card

---
+++ src/components/ContactUsTile.jsx
@@ -0,0 +1,15 @@
+import "./ContactUsTile.css";
+
+function ContactUsTile() {
+    return (
+        <div className="contact-us-tile">
+            <h4 className="contact-title">Contact Us</h4>
+
+            <p className="contact-email">
+                playpalapp2026@gmail.com
+            </p>
+        </div>
+    );
+}
+
+export default ContactUsTile;
